01158946ab122ac892e7ec0fa6f09a6cce578a11
Otto <you@example.com>
2026-04-21 15:07:06 +0300

    navigation

--- public/index.php
+++ public/index.php
@@ -5,8 +5,24 @@ $dotenv->load();
 
 require __DIR__ . '/../db/DB.php';
 require_once __DIR__ . '/../src/controllers/CustomerController.php';
+require_once __DIR__ . '/../src/controllers/OrderController.php';
+require_once __DIR__ . '/../src/controllers/HomeController.php';
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if ($path === '/customers' || $path === '/') {
-    CustomerController::index();
+
+switch ($path) {
+    case '/':
+    case '':
+        HomeController::index();
+        break;
+    case '/customers':
+        CustomerController::index();
+        break;
+    case '/orders':
+        OrderController::index();
+        break;
+    default:
+        http_response_code(404);
+        echo "404 Not Found";
+        break;
 }
---
+++ src/views/nav.php
@@ -0,0 +1,5 @@
+<nav class="main-nav">
+    <a href="/">Home</a>
+    <a href="/customers">Customers</a>
+    <a href="/orders">Orders</a>
+</nav>
